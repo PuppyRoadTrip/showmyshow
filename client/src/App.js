10b45e4bc08702455e55cf30a2292c0d90e1dd4b
Pro-Code-art <jayme.coder@gmail.com>
2021-04-10 14:22:59 -0700

    set up pages and router in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "../src/pages/Home";
+import Login from "../src/pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+      <Link to="./"> Home</Link>
+      <Link to="./login"> Login</Link>
+      <Home/>
+      <Login/>
+    </Router>
   );
 }
-
 
 export default App;
