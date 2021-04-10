4d13e517b680d22c33f3a0b02470db021d60d75e
Pro-Code-art <jayme.coder@gmail.com>
2021-04-10 12:13:38 -0700

    set up server and api routes for user according to schema

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express = require("express");
+const morgan = require('morgan')
 const path = require("path");
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const UserApiRoutes = require('./routes/UserApi');
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('tiny'))
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+app.use('/api/User', UserApiRoutes)
+
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/showmyshow");
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+});   
