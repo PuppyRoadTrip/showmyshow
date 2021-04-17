ca1b7597a858e2fb2df2c7cd8fc00bf9c35b82d6
Josh Rehanek <joshrehanek@gmail.com>
2021-04-17 10:53:28 -0700

    adds working Twitter Card render

--- client/src/components/TwitterCard/TwitterCard.js
+++ client/src/components/TwitterCard/TwitterCard.js
@@ -1,21 +1,20 @@
 import React from "react";
 
 export default function TwitterCards({ tweets }) {
-  console.log("This is the TwitterCards console" + tweets);
-  return tweets.map((tweet) => <TwitterCard key={tweet.id} tweet={tweet} />);
+  console.log("This is the TwitterCards console", tweets);
+  return tweets.map((tweet, i) => <TwitterCard key={i} tweet={tweet} />);
 }
 
 function TwitterCard({ tweet }) {
-  console.log("This is the TwitterCard console log" + tweet);
   return (
-    <div class="row">
-      <div class="col s12 m6">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title">
-              Twitter Handle: {tweet.user.screen_name}
+    <div className="row">
+      <div className="col s12 m6">
+        <div className="card blue-grey darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">
+              Twitter Handle: {tweet.screen_name}
             </span>
-            <span class="card-title">Tweet: {tweet.text}</span>
+            <span className="card-title">Tweet: {tweet.text}</span>
           </div>
         </div>
       </div>
