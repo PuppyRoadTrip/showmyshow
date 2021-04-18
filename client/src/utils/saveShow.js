8fa14c374350d67f4c4b3c8db5b457f681ae6995
Pro-Code-art <jayme.coder@gmail.com>
2021-04-17 18:59:11 -0700

    debugged issue with econnrefused - env issue and confirmed axios call is working

--- client/src/utils/saveShow.js
+++ client/src/utils/saveShow.js
@@ -9,14 +9,3 @@ export default {
   },
 };
 
-//Old Fetch call that was working
-//   const saveShow = async (e) => {
-//       e.preventDefault();
-//       const requestOptions = {
-//         method: "POST",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify({ show: eventsState[e.target.id] }),
-//       };
-//       await fetch("/api/save", requestOptions);
-//       window.location.replace("/saved");
-//     };
