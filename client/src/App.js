ad56b4d32f1152aefc1dc9bea46d4163bcef6f66
Josh Rehanek <joshrehanek@gmail.com>
2021-04-21 12:35:25 -0700

    adds chatroom home and chatroom to router switch

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,20 @@ import Home from '../src/pages/Home';
 import Login from '../src/pages/Login';
 import AccountLanding from '../src/pages/AccountLanding';
 import SavedShows from './pages/SavedShows';
+import ChatRoomHome from './pages/ChatRoomHome/Home';
+import ChatRoom from './pages/ChatRoom/ChatRoom';
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/landing" component={AccountLanding} />
-          <Route exact path="/saved" component={SavedShows} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/landing' component={AccountLanding} />
+          <Route exact path='/saved' component={SavedShows} />
+          <Route exact path='/chat' component={ChatRoomHome} />
+          <Route exact path='/:roomId' component={ChatRoom} />
         </Switch>
       </Router>
     </>
