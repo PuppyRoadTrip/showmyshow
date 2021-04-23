34799f519d94e834e1cd5db00de25c60e042d000
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-04-23 16:33:48 -0700

    modularize user email information, fix display to be displayed next to user avatar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from '../src/pages/Login';
@@ -8,36 +8,26 @@ import ChatRoomHome from './pages/ChatRoomHome/Home';
 import ChatRoom from './pages/ChatRoom/ChatRoom';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
-import { Auth } from 'aws-amplify';
+import { AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
+
 
 Amplify.configure(awsconfig);
 
 function App() {
-  const [userState, setUserState] = useState('');
 
-  useEffect(() => {
-    Auth.currentAuthenticatedUser().then((user) => {
-      if (user.attributes.email === userState) return;
-      setUserState(user.attributes.email);
-      console.log('Current user is: ', user.attributes.email);
-    });
-  });
-
-  return (
-    <>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomeLanding} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/saved" component={SavedShows} />
-          <Route exact path="/chat" component={ChatRoomHome} />
-          <Route exact path="/:roomId" component={ChatRoom} />
-        </Switch>
-      </Router>
-      <AmplifySignOut />
-    </>
-  );
+return (
+  <>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomeLanding} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/saved" component={SavedShows} />
+        <Route exact path="/chat" component={ChatRoomHome} />
+        <Route exact path="/:roomId/:username" component={ChatRoom} />
+      </Switch>
+    </Router>
+    <AmplifySignOut />
+  </>
+);
 }
-
 export default withAuthenticator(App);
