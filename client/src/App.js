62b5b46b1d8fde96b032535cda35780a694a6bf7
Josh Rehanek <joshrehanek@gmail.com>
2021-04-25 23:21:20 -0700

    corrects ChatRoom route

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
           <Route exact path="/login" component={Login} />
           <Route exact path="/saved" component={SavedShows} />
           <Route exact path="/chat" component={ChatRoomHome} />
-          <Route exact path="/:roomId/:username" component={ChatRoom} />
+          <Route exact path="/:roomId" component={ChatRoom} />
         </Switch>
       </Router>
       <div className="row" id="sign-out-row">
