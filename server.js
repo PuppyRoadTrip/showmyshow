b75d219a3dbb2607922e0d92e3b5d32897d8ce00
Josh Rehanek <joshrehanek@gmail.com>
2021-04-28 13:04:43 -0700

    Deploy Attempt

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use('/api/twitter', TwitterAPIRoutes)
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/public/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/showmyshow',  {
